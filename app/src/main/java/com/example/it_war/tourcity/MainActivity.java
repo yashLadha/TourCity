07cefa3071e02fdd20773ef3a802836155bc68dd
RanchoDbest <rachanasolanki311@gmail.com>
2017-04-13 14:03:55 +0530

    Initial Code added

---
+++ app/src/main/java/com/example/it_war/tourcity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.it_war.tourcity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
